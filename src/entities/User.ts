e0de23d863ec6d71aabd8df00efbb73e549ffba3
takm-reason <takm.reason@gmail.com>
2019-08-24 16:13:01 +0900

    add moment at entities

--- src/entities/User.ts
+++ src/entities/User.ts
@@ -1,8 +1,12 @@
+import moment from 'moment-timezone';
 
 export interface IUser {
     id?: number;
     name: string;
     email: string;
+    createdAt?: moment.Moment;
+    updatedAt?: moment.Moment;
+    deletedAt?: moment.Moment;
 }
 
 export class User implements IUser {
@@ -10,6 +14,9 @@ export class User implements IUser {
     public id?: number;
     public name: string;
     public email: string;
+    public createdAt?: moment.Moment;
+    public updatedAt?: moment.Moment;
+    public deletedAt?: moment.Moment;
 
     constructor(nameOrUser: string | IUser, email?: string) {
         if (typeof nameOrUser === 'string') {
